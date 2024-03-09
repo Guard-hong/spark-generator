ea0a2a944b61bea6289023a4b295c7849601e425
Guard-hong <h334_6343@163.com>
2024-03-09 18:00:49 +0800

    项目准备

---
+++ spark-generator-basic/src/main/java/com/hcj/Main.java
@@ -0,0 +1,12 @@
+package com.hcj;
+
+/**
+ * @Author:HCJ
+ * @DateTime:2024/3/9
+ * @Description:
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
